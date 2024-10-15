8be192abe3749df0cb1a1b2651776f47224f2fad
Chipper-03 <ckj0018@auburn.edu>
2024-10-15 18:28:59 -0500

    Update Hey_Day_Script.sh

--- Hey_Day_Script.sh
+++ Hey_Day_Script.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+# Script greets the user and asks them to input a name
+echo "Hello! I am so excited to meet you! What is your name?"
+read name
+
+# The read command will take what the user inputs and saves it under the variable name
+
+echo "Hey $name! It is so awesome to meet you!"
